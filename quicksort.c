5453cb9a1cd98e85526ce8f8d7540d4ac8ec6767
HLahoti <44701898+HLahoti@users.noreply.github.com>
2024-02-04 10:57:05 +0530

    Update quicksort.c

--- quicksort.c
+++ quicksort.c
@@ -5,50 +5,7 @@
 #include <time.h>
 
 /*
-/ 7
-
-! add -g -pthread to your gcc command
-* be extremely careful with shared memory in threads
-* threads may try to modify the same memory
-* that is called a race condition
-! gcc -S for assembly compilation
-
-/ Mutex
-Protects from race conditions.
-Does not allow any other thread to execute the code
-inside the locked region.
-*Mutex works with more than 2 threads too.
-Try not to use it too often, it is a little slow.
-
-! never create and join threads in the same for loop,
-! that will only mean thre is no parallel execution.
-
-/ Returning values
-cant retrurn local variables ofc, so we return pointer
-then free the pointer later
-
-/ Passing arguments
-Since you're only passing memory locations,
-you have to be careful
-
-/ We use conditions to control thread execution
-pthread_cond_wait calls the unlock method and
-waits till a signal is send back using the same
-cond_t object. Once that signal is recieved
-the lock method is again called and execution resumes
-It is important to keep pthread_cond_wait inside a
-condition loop.
-!pthread_cond_signal signals at most 2 waits
-
-/ Monte carlo appraoch, but multithreaded
-1.  We use the monte carlo appraoch to approximate the value of pi
-    The program generates n random points inside asquare (side 2*r)
-    and then counts how many are inside the circle (which has radius r)
-2.  pi = 4 * (number of points inside the circle)/(total number of points)
-    This program uses OpenMP for achieving parallel computing.
-3.  The program only requires C++ for random numbers, otherwise using C is fine.
-
-/ Quicksort, multithreaded
+Quicksort, multithreaded
 1.  An array is partitioned around a pivot element
     In this case, the last element of any subarray
 2.  The array is partitioned into 2 parts, left smaller than pivot
